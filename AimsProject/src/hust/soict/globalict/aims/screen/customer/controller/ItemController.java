aa06ad28e070459d11c87d1c12f9ea973d72d05d
Hieu Hoang <hieuhieuhieu12@gmail.com>
2024-05-27 14:37:28 +0700

    committed at 14:37 5-27-2024

---
+++ AimsProject/src/hust/soict/globalict/aims/screen/customer/controller/ItemController.java
@@ -0,0 +1,56 @@
+package hust.soict.globalict.aims.screen.customer.controller;
+
+import hust.soict.globalict.aims.cart.Cart;
+import hust.soict.globalict.aims.media.Media;
+import hust.soict.globalict.aims.media.Playable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class ItemController {
+	private Media media;
+
+    @FXML
+    private Button btnAddToCart;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblCost;
+
+    @FXML
+    void btnAddToCartClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlayClicked(ActionEvent event) {
+
+    }
+    
+    public void setData(Media media) {
+    	this.media = media;
+    	lblTitle.setText(media.getTitle());
+    	lblCost.setText("$ " + media.getCost());
+    	if(media instanceof Playable) {
+    		btnPlay.setVisible(true);
+    	}
+    	else {
+    		btnPlay.setVisible(false);
+    		HBox.setMargin(btnAddToCart, new Insets(0, 0, 0, 60));
+    	}
+    }
+    
+    public ItemController(Cart cart) {
+    	
+    }
+
+}
+
